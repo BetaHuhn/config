253559ca6ddb5e150f3a5aff90e7de8534f36eb4
BetaHuhn <schiller@mxis.ch>
2021-04-13 12:02:45 +0200

    ✨ Add ts eslint config

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import { master, production, release } from "./config/release.config";
 import eslint from "./config/eslint.config";
 import eslintVue from "./config/eslintVue.config";
+import eslintTypescript from "./config/eslintTs.config";
 
 const releaseMaster = master;
 const releaseProduction = production;
 
-export { eslint, eslintVue, release, releaseMaster, releaseProduction };
+export { eslint, eslintVue, eslintTypescript, release, releaseMaster, releaseProduction };
